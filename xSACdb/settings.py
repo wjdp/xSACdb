da19a655d800fe8f8171fd3d32ab0dda1ed59537
Will Pimblett <will@fullaf.com>
2013-03-19 02:15:54 +0000

    added members, training and trips apps

--- xSACdb/settings.py
+++ xSACdb/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3, # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -117,6 +117,12 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+
+    'south',
+
+    'xsd_members',
+    'xsd_training',
+    'xsd_trips',
 )
 
 # A sample logging configuration. The only tangible logging
