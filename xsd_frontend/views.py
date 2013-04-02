37712301f9facea78d93998b1ce6148c2a7abaf5
Will Pimblett <will@fullaf.com>
2013-04-02 12:53:07 +0100

    added 'hello world' dashboard to root /

--- xsd_frontend/views.py
+++ xsd_frontend/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def dashboard(request):
-    return render(request,'xsd_frontend/dashboard.html')
+    return render(request,'xsd_frontend/dashboard.html', {'request':request})
