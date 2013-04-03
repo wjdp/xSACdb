7f7d74a6f2e18061a783b73e928204ebb4c48af5
Will Pimblett <will@fullaf.com>
2013-04-03 03:28:59 +0100

    Implemented first stages of hiding UI elements based on permissions Uses a context processor to achive this

---
+++ xSACdb/context_processors.py
@@ -0,0 +1,9 @@
+def menu_perms(request):
+    u=request.user
+    has_admin_module=u.is_superuser
+    has_members_module=u.has_perm('xsd_members.add_memberprofile')
+
+    return {
+        'has_admin_module':has_admin_module,
+        'has_members_module':has_members_module,
+    }
--- xsd_frontend/views.py
+++ xsd_frontend/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 def dashboard(request):
-    return render(request,'xsd_frontend/dashboard.html', {'request':request})
+    return render(request,'xsd_frontend/dashboard.html', {'request':request}, context_instance=RequestContext(request))
