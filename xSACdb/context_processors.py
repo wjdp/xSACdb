5bc41d2158d09d18953bbd55e3630926219ef146
Will Pimblett <will@fullaf.com>
2013-07-11 16:55:45 +0100

    sensible current page detection

--- xSACdb/context_processors.py
+++ xSACdb/context_processors.py
@@ -1,14 +1,20 @@
+from django.core.urlresolvers import resolve
+
+
 def menu_perms(request):
     if request.user.is_authenticated():
         u=request.user
         p=u.get_profile()
         has_admin_module=u.is_superuser
         has_members_module=u.has_perm('xsd_members.add_memberprofile')
+        current_url = resolve(request.path_info).url_name
         return {
             'request': request,
             'user': u,
             'profile': p,
+            'current_url':current_url,
             'has_admin_module':has_admin_module,
             'has_members_module':has_members_module,
         }
     else: return {}
+
