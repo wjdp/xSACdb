b6af900a26f3a65984c951d86d38093d56d43011
Will Pimblett <will@fullaf.com>
2014-10-27 18:39:13 +0000

    show instructors on database officers page

--- xsd_about/views.py
+++ xsd_about/views.py
@@ -17,13 +17,15 @@ class DatabaseOfficersView(RequireVerified, TemplateView):
 	template_name='about_database_officers.html'
 	def get_users_in_role(self, role):
 		return Group.objects.get(pk=role).user_set.all()
+	def get_instructors(self):
+		return User.objects.filter(memberprofile__is_instructor_cached = True)
 	def get_context_data(self, **kwargs):
 	    context = super(DatabaseOfficersView, self).get_context_data(**kwargs)
 	    context['admins'] = self.get_users_in_role(2)
 	    context['dos'] = self.get_users_in_role(7)
 	    context['mos'] = self.get_users_in_role(6)
 	    context['tos'] = self.get_users_in_role(3)
-	    context['instructors'] = None
+	    context['instructors'] = self.get_instructors()
 	    context['tas'] = self.get_users_in_role(4)
 	    context['sas'] = self.get_users_in_role(5)
 	    return context
