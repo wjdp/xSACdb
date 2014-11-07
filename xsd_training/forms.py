9423697014597d218b320fd1603e46dc93748dfe
Will Pimblett <will@fullaf.com>
2014-11-07 21:53:17 +0000

    award sdc view #12

--- xsd_training/forms.py
+++ xsd_training/forms.py
@@ -14,26 +14,31 @@ class PerformedSDCCreateForm(forms.ModelForm):
         exclude = ('trainees', 'completed')
 
 class PerformedSDCUpdateForm(forms.ModelForm):
-	class Meta:
-		model=PerformedSDC
-		fields= ('sdc', 'datetime', 'notes')
+    class Meta:
+        model=PerformedSDC
+        fields= ('sdc', 'datetime', 'notes')
 
 class QualificationSelectForm(forms.Form):
-	qualification=forms.ModelChoiceField(queryset=Qualification.objects.all())
-	selected_members=forms.ModelMultipleChoiceField(
-		queryset=MemberProfile.objects.all())
+    qualification=forms.ModelChoiceField(queryset=Qualification.objects.all())
+    selected_members=forms.ModelMultipleChoiceField(
+        queryset=MemberProfile.objects.all())
+
+class SDCSelectForm(forms.Form):
+    sdc=forms.ModelChoiceField(queryset=SDC.objects.all())
+    selected_members=forms.ModelMultipleChoiceField(
+        queryset=MemberProfile.objects.all())
 
 class SessionCreateForm(forms.ModelForm):
-	class Meta:
-		model=Session
-		fields=['name', 'when','where','notes']
+    class Meta:
+        model=Session
+        fields=['name', 'when','where','notes']
         widgets = {
             'when': AdminDateWidget(),
         }
 
-class SessionPLMapForm(forms.ModelForm):	# Used for mapping trainees to lessons and instructors in a Session
+class SessionPLMapForm(forms.ModelForm):    # Used for mapping trainees to lessons and instructors in a Session
     def __init__(self,*args,**kwargs):
-    	super(SessionPLMapForm, self).__init__(*args,**kwargs)
+        super(SessionPLMapForm, self).__init__(*args,**kwargs)
 
         # Exclude experience dives from the selection
         self.fields['lesson'].queryset=Lesson.objects.exclude(mode='XP').order_by('qualification','mode')
