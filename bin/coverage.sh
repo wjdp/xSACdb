95739c331292b5100cd4c4ac3d114388aab9a90c
Will Pimblett <will@fullaf.com>
2015-10-24 09:31:43 +0100

    Ignore migrations in coverage report

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -3,7 +3,7 @@
 # Local test suite
 
 source env/bin/activate
-coverage run --include=src/* src/manage.py test xSACdb xsd_about xsd_auth xsd_frontend xsd_help xsd_kit xsd_members xsd_sites xsd_training xsd_trips
+coverage run --include=src/* --omit=*/migrations/*  src/manage.py test xSACdb xsd_about xsd_auth xsd_frontend xsd_help xsd_kit xsd_members xsd_sites xsd_training xsd_trips
 mkdir -p tmp/
 coverage html -d tmp/htmlcov
 xdg-open tmp/htmlcov/index.html
