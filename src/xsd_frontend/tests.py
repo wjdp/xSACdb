454047d9be154f9f40e83fc06a1ab84da4edeed6
Will Pimblett <will@fullaf.com>
2016-05-15 23:59:41 +0100

    Test registration view, closes #152

--- src/xsd_frontend/tests.py
+++ src/xsd_frontend/tests.py
@@ -1,25 +1,52 @@
 from django.test import TestCase
 from django.test.client import Client
-from django.conf import settings
-
-from django.contrib.auth import get_user_model
 
 from django.core.urlresolvers import reverse
 
+from xsd_auth.models import User
+
 from xSACdb.test_helpers import BaseTest
 
+import testdata
+
 class AccountsLogin(TestCase):
-    def test_login_form_alive(self):
+    def test_200(self):
         c=Client()
-        response = c.get('/accounts/login/')
-        self.assertEqual(response.status_code, 200)
+        r = c.get('/accounts/login/')
+        self.assertEqual(r.status_code, 200)
 
 
 class RegisterLogin(TestCase):
-    def test_register_form_alive(self):
+    def test_200(self):
         c=Client()
-        response = c.get('/accounts/register/')
-        self.assertEqual(response.status_code, 200)
+        r = c.get('/accounts/register/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_register_form(self):
+        c = Client()
+        password = testdata.get_str(str_size=128);
+        post_data = {
+            'first_name': testdata.get_name(name_count=2),
+            'last_name': testdata.get_name(name_count=2),
+            'email_address': testdata.get_email(),
+            'password': password,
+            'password_again': password,
+        }
+        r = c.post('/accounts/register/', post_data)
+
+        # Check user exists
+        self.assertEqual(User.objects.filter(email=post_data['email_address']).count(), 1)
+
+        # Check user creation
+        u = User.objects.get(email=post_data['email_address'])
+        self.assertEqual(u.first_name, post_data['first_name'])
+        self.assertEqual(u.last_name, post_data['last_name'])
+        self.assertEqual(u.email, post_data['email_address'])
+
+        # Check profile creation
+        self.assertEqual(u.memberprofile.first_name, post_data['first_name'])
+        self.assertEqual(u.memberprofile.last_name, post_data['last_name'])
+        self.assertEqual(u.memberprofile.email, post_data['email_address'])
 
 class ClassicLogin(BaseTest):
     def test_login_username(self):
