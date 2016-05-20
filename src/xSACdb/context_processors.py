1647d7fc5032c19070640d2d51ef29ffdda8e823
Will Pimblett <will@fullaf.com>
2016-05-20 01:13:19 +0100

    Do active tracking in main nav

--- src/xSACdb/context_processors.py
+++ src/xSACdb/context_processors.py
@@ -21,11 +21,16 @@ def menu_perms(request):
         DEBUG = settings.DEBUG
         l10n_club = settings.CLUB
 
+        current_url_obj = resolve(request.path)
+        namespace = current_url_obj.namespaces[0]
+
         return {
             'request': request,
             'user': u,
             'profile': p,
-            'current_url':current_url,
+            'current_url':current_url, # TODO remove?
+            'current_url_obj':current_url_obj,
+            'namespace': namespace,
 
             'DEBUG': DEBUG,
             'l10n_club': l10n_club,
