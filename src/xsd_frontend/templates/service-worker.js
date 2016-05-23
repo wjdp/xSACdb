c85bdb61d7461d299e0d0c89ad4f4dbf81f2e032
Will Pimblett <will@fullaf.com>
2016-05-23 01:59:37 +0100

    Add Manifest and ServiceWorker
    don't do much currently

---
+++ src/xsd_frontend/templates/service-worker.js
@@ -0,0 +1,1 @@
+/* Blank service worker */
--- src/xsd_frontend/urls.py
+++ src/xsd_frontend/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
+from django.views.generic import TemplateView
 
 from views import *
 
 urlpatterns = patterns('',
     url(r'^$', 'xsd_frontend.views.dashboard', name='dashboard'),
-    url(r'^activity/$', ActivityTable.as_view(), name='activity_table'),
+    url(r'activity/$', ActivityTable.as_view(), name='activity_table'),
     #url(r'^activity/feed/$', ActivityFeed.as_view(), name='activity_feed'),
+
+    url(r'manifest\.json$', TemplateView.as_view(
+            template_name='manifest.json',
+            content_type='application/json'),
+        name='app-manifest'
+    ),
+    url(r'service-worker\.js$', TemplateView.as_view(
+            template_name='service-worker.js',
+            content_type='text/javascript'),
+        name='app-manifest'),
 )
