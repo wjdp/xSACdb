1ce0fea1d1eb6f442b09959de234749af43d8d5d
Will Pimblett <will@fullaf.com>
2016-05-25 07:24:36 +0100

    Make tests use unicode lits, add forgotten migration

--- src/xsd_sites/tests.py
+++ src/xsd_sites/tests.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import random
 
 from xSACdb.test_helpers import BaseTest
--- src/xsd_training/tests.py
+++ src/xsd_training/tests.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.core.urlresolvers import reverse
 
 from xSACdb.test_helpers import BaseAsGroupTest, BaseTest
