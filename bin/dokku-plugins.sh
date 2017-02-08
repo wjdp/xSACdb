64eeee96ab2e39de2886dbbcfe869039b6e17e0c
Will Pimblett <will@wjdp.uk>
2017-02-08 01:29:42 +0000

    Add dokku-maintenance to dokku plugins script

--- bin/dokku-plugins.sh
+++ bin/dokku-plugins.sh
@@ -4,3 +4,4 @@ sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres
 sudo dokku plugin:install https://github.com/dokku/dokku-redis.git redis
 sudo dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
 dokku letsencrypt:cron-job --add
+sudo dokku plugin:install https://github.com/dokku/dokku-maintenance.git
