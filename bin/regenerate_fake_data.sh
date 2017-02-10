065eebb47b8a5ccad6ec73344adefd0f32e3c6a0
Will Pimblett <will@wjdp.uk>
2017-02-10 21:38:17 +0000

    Show errors at top of MemberUpdate view
    Re #220. Again not ideal but fixes minor usability issue until we re-do that page.

--- bin/regenerate_fake_data.sh
+++ bin/regenerate_fake_data.sh
@@ -4,5 +4,6 @@ source env/bin/activate
 rm tmp/db.sqlite3
 src/manage.py migrate
 src/manage.py loaddata groups
+src/manage.py loaddata membershiptypes
 src/manage.py loaddata tmp/bsac_data.yaml
 src/manage.py generate_fake_data
