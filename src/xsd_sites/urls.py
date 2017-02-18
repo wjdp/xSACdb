0d0311c2490ade598627ac11d951e6a314433c9f
Will Pimblett <will@wjdp.uk>
2017-02-18 21:33:59 +0000

    Add history to PSDC, Session, TGs and Sites

--- src/xsd_sites/urls.py
+++ src/xsd_sites/urls.py
@@ -1,13 +1,17 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
+from django.conf.urls import patterns, url
 
 from views import *
+from models import Site
+from xsd_frontend.versioning import VersionHistoryView
 
 urlpatterns = patterns('',
-        url(r'^$', SitesOverview.as_view(), name='SitesOverview'),
-        url(r'^search/$', SitesSearch.as_view(), name='SitesSearch'),
-        url(r'^add/$', SiteCreate.as_view(), name='SiteCreate'),
-        url(r'^edit/$', SitesList.as_view(), name='SitesList'),
-        url(r'^edit/(?P<pk>\d+)/$', SiteUpdate.as_view(), name='SiteUpdate'),
-        url(r'^json/(?P<pk>\d+)/$', 'xsd_sites.views.sitedetail_json', name='sitedetail_json'),
+    url(r'^$', SitesOverview.as_view(), name='SitesOverview'),
+    url(r'^search/$', SitesSearch.as_view(), name='SitesSearch'),
+    url(r'^add/$', SiteCreate.as_view(), name='SiteCreate'),
+    url(r'^edit/$', SitesList.as_view(), name='SitesList'),
+    url(r'^edit/(?P<pk>\d+)/$', SiteUpdate.as_view(), name='SiteUpdate'),
+    url(r'^edit/(?P<pk>\d+)/history/$', VersionHistoryView.as_view(), name='SiteHistory',
+       kwargs={'model': Site}),
+
+    url(r'^json/(?P<pk>\d+)/$', 'xsd_sites.views.sitedetail_json', name='sitedetail_json'),
 )
