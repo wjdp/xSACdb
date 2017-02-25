6d713184212f9c8cc786270a847fa3cb42abb31a
Will Pimblett <will@wjdp.uk>
2017-02-25 13:27:54 +0000

    Don't run init_scheduler in TEST

--- src/xsd_frontend/apps.py
+++ src/xsd_frontend/apps.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.conf import settings
 from health_check.plugins import plugin_dir
 
 from xSACdb.health.rq import RQWorkerHealthCheck, RQSchedulerHealthCheck
@@ -14,4 +15,5 @@ class FrontendConfig(AppConfig):
         # This really is a backend thing, but as we bundle backend with the project dir can only be done here
         plugin_dir.register(RQWorkerHealthCheck)
         plugin_dir.register(RQSchedulerHealthCheck)
-        init_scheduler()
+        if settings.ENVIRONMENT in ('DEVELOPMENT', 'PRODUCTION'):
+            init_scheduler()
