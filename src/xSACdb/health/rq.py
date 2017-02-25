7cc47dc632f39174f9236c93d75d2cae81c7de57
Will Pimblett <will@wjdp.uk>
2017-02-25 00:49:07 +0000

    Add schedule_once scheduler, run on app boot
    Single test job setup, should run every minute
    Move RQ status to /admin/rq/ from /about

--- src/xSACdb/health/rq.py
+++ src/xSACdb/health/rq.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import time
 
 from django.conf import settings
-from django_rq import enqueue
+from django_rq import enqueue, get_scheduler
 from health_check.backends import BaseHealthCheckBackend
 from health_check.exceptions import ServiceReturnedUnexpectedResult, ServiceUnavailable
 
@@ -20,10 +20,23 @@ class RQWorkerHealthCheck(BaseHealthCheckBackend):
             while True:
                 if job.result or time.time() > timeout:
                     break
-            if job.result == None:
+            if not job.result:
                 self.add_error(ServiceUnavailable("No result, are workers running?"))
             elif job.result == '11':
                 self.add_error(ServiceReturnedUnexpectedResult("Invalid result"))
         except BaseException as e:
             self.add_error(ServiceUnavailable("Unknown error"), e)
 
+
+class RQSchedulerHealthCheck(BaseHealthCheckBackend):
+    def check_status(self):
+        try:
+            scheduler = get_scheduler('default')
+            if not scheduler:
+                self.add_error(ServiceUnavailable("No scheduler"))
+            jobs = scheduler.get_jobs()
+            if len(jobs) == 0:
+                self.add_error(ServiceUnavailable("No jobs in scheduler"))
+        except BaseException as e:
+            self.add_error(ServiceUnavailable("Unknown error"), e)
+            raise(e)
--- src/xsd_frontend/apps.py
+++ src/xsd_frontend/apps.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 from health_check.plugins import plugin_dir
 
-from xSACdb.health.rq import RQWorkerHealthCheck
-
+from xSACdb.health.rq import RQWorkerHealthCheck, RQSchedulerHealthCheck
+from xSACdb.scheduler import init_scheduler
 
 class FrontendConfig(AppConfig):
     name = 'xsd_frontend'
@@ -13,3 +13,5 @@ class FrontendConfig(AppConfig):
     def ready(self):
         # This really is a backend thing, but as we bundle backend with the project dir can only be done here
         plugin_dir.register(RQWorkerHealthCheck)
+        plugin_dir.register(RQSchedulerHealthCheck)
+        init_scheduler()
