44a8589dc8d2d01acd909a89f854782ac48cdaa4
Will Pimblett <will@wjdp.uk>
2017-03-22 17:32:06 +0000

    Correct help text on setuo_scheduler management command

--- src/xsd_frontend/management/commands/setup_scheduler.py
+++ src/xsd_frontend/management/commands/setup_scheduler.py
@@ -4,9 +4,7 @@ from xSACdb.scheduler import init_scheduler
 
 
 class Command(BaseCommand):
-    """A simple management command which clears the site-wide cache."""
-    # Taken from https://github.com/rdegges/django-clear-cache/blob/master/clear_cache/management/commands/clear_cache.py
-    help = 'Fully clear your site-wide cache.'
+    help = 'Sets-up scheduled tasks. Should be called when application is deployed and after clearing the cache.'
 
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.MIGRATE_HEADING('Setting up scheduled tasks...'))
