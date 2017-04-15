1cc6b29b36af93897cee510009ec95f5321587e2
Will Pimblett <will@wjdp.uk>
2017-04-15 15:42:01 +0100

    Refactored trainee detail BS4.a6
    Minimum to get functional
    
    Add xsd-qual-fg classes for foreground colouring
    Refactored profile-card into xsd-profile-card component.

--- src/xsd_training/templatetags/report_extras.py
+++ src/xsd_training/templatetags/report_extras.py
@@ -1,13 +1,16 @@
+from __future__ import unicode_literals
+
 from django import template
 
 from xsd_training.models import *
 
 register = template.Library()
 
+
 def build_lesson_row(lessons, mp):
     lesson_row = ""
     for lesson in lessons:
-        pls = PerformedLesson.objects.filter(lesson = lesson, trainee = mp)
+        pls = PerformedLesson.objects.filter(lesson=lesson, trainee=mp)
         if lesson.qualification != mp.training_for:
             continue
         completed = False
@@ -16,16 +19,23 @@ def build_lesson_row(lessons, mp):
         for pl in pls:
             if pl.completed: completed = True
             if pl.partially_completed: partially_completed = True
-        if lesson.code: code_o = lesson.code
-        else: code_o = lesson.title
-        lesson_row += '<td data-l='+str(lesson.pk)+" data-t="+str(mp.pk)+" data-q="+str(lesson.qualification.pk)
-        if completed:                lesson_row += ' class="ljs completed">'
-        elif partially_completed:    lesson_row += ' class="ljs partially_completed">'
-        else:                        lesson_row += ' class="ljs nothing">'
-        lesson_row += code_o+'</td>'
-    if lesson_row == "": return ""
-    else: return "<tr><th>" + mode + "</th>" + lesson_row + "</tr>"
-
+        if lesson.code:
+            code_o = lesson.code
+        else:
+            code_o = lesson.title
+        lesson_row += '<td data-l=' + str(lesson.pk) + " data-t=" + str(mp.pk) + " data-q=" + str(
+            lesson.qualification.pk)
+        if completed:
+            lesson_row += ' class="ljs completed">'
+        elif partially_completed:
+            lesson_row += ' class="ljs partially_completed">'
+        else:
+            lesson_row += ' class="ljs nothing">'
+        lesson_row += code_o + '</td>'
+    if lesson_row == "":
+        return ""
+    else:
+        return "<tr class='mode--{0}'>\n<th>{0}</th>\n{1}\n</tr>".format(mode, lesson_row)
 
 
 @register.simple_tag
@@ -37,11 +47,10 @@ def show_lessons(mp, only_main_three=False):
     for mode in Lesson.MODE_CHOICES:
         lessons[mode[0]] = Lesson.objects.filter(mode=mode[0]).order_by('order').prefetch_related('qualification')
 
-    output = '<table class=" progress-table">\n'
+    output = '<table class="xsd-pl-matrix">\n'
     for mode in Lesson.MODE_CHOICES:
         output += build_lesson_row(lessons[mode[0]], mp)
 
     output += "</table>"
 
     return output
-
