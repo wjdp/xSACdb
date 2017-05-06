31e5a89e7a61a063b6f3908a73cb1b0a2833a455
Will Pimblett <will@wjdp.uk>
2017-05-06 18:17:10 +0100

    Implement set and award qualification methods

--- src/xsd_members/models/profile_qualification.py
+++ src/xsd_members/models/profile_qualification.py
@@ -1,11 +1,19 @@
+from __future__ import unicode_literals
+
+import reversion
+
+from xsd_frontend.activity import DoAction
+from xsd_training.models import PerformedQualification
+
+
 class MemberProfileQualificationMixin(object):
-    def award_qualification(self, qualification, actor=None):
-        pass
+    def award_qualification(self, performed_qualification, actor=None):
+        performed_qualification.trainee = self
 
-    # def add_qualification(self, qualification):
-    #     pass
-    #
+        with DoAction() as action, reversion.create_revision():
+            performed_qualification.save()
+            reversion.revisions.set_user(actor)
+            action.set(actor=actor, verb='added qualification', target=self, style='qualification-awarded')
 
-    # def remove_qualification(self, qualification):
-    #     pass
-    #
+    def set_qualification(self, qualification, notes='Created with set_qualification'):
+        PerformedQualification(trainee=self, qualification=qualification, mode='OTH', notes=notes).save()
