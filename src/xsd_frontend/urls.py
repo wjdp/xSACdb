acac393adcddc00a2405f716b2ec22ddaf4d9ba4
Will Pimblett <will@wjdp.uk>
2017-05-07 22:56:51 +0100

    Update urls.py files re. django depreciations
    No more string refs to FBVs, patterns func to plain list.

--- src/xsd_frontend/urls.py
+++ src/xsd_frontend/urls.py
@@ -1,33 +1,38 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import RedirectView
-from django.views.generic import TemplateView
+from __future__ import unicode_literals
+
+from django.conf.urls import url
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.views.generic import RedirectView
 
 from views import *
 
 favicon_view = RedirectView.as_view(url=static('icons/favicon.ico'), permanent=True)
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', DashboardView.as_view(), name='dashboard'),
 
     url(r'^accounts/login/$', PreauthLoginView.as_view(), name='login'),
     url(r'^accounts/register/$', PreauthRegisterView.as_view(), name='register'),
-    url(r'^accounts/logout/$', 'xsd_frontend.views.logout', name='logout'),
+    url(r'^accounts/logout/$', logout, name='logout'),
+
+    url(r'^update-request/$', update_request, name='update_request'),
+
+    url(r'^design/$', design, name='design'),
 
     url(r'^favicon\.ico$', favicon_view),
     url(r'manifest\.json$', TemplateView.as_view(
-            template_name='browser/manifest.json',
-            content_type='application/json'),
+        template_name='browser/manifest.json',
+        content_type='application/json'),
         name='app-manifest'
-    ),
+        ),
     url(r'browserconfig\.xml', TemplateView.as_view(
-            template_name='browser/browserconfig.xml',
-            content_type='application/xml'),
+        template_name='browser/browserconfig.xml',
+        content_type='application/xml'),
         name='app-browserconfig'
-    ),
+        ),
 
     url(r'service-worker\.js$', TemplateView.as_view(
-            template_name='service-worker.js',
-            content_type='text/javascript'),
+        template_name='service-worker.js',
+        content_type='text/javascript'),
         name='app-service-worker'),
-)
+]
--- src/xsd_sites/urls.py
+++ src/xsd_sites/urls.py
@@ -1,17 +1,19 @@
-from django.conf.urls import patterns, url
+from __future__ import unicode_literals
 
-from views import *
+from django.conf.urls import url
+
 from models import Site
+from views import *
 from xsd_frontend.versioning import VersionHistoryView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', SitesOverview.as_view(), name='SitesOverview'),
     url(r'^search/$', SitesSearch.as_view(), name='SitesSearch'),
     url(r'^add/$', SiteCreate.as_view(), name='SiteCreate'),
     url(r'^edit/$', SitesList.as_view(), name='SitesList'),
     url(r'^edit/(?P<pk>\d+)/$', SiteUpdate.as_view(), name='SiteUpdate'),
     url(r'^edit/(?P<pk>\d+)/history/$', VersionHistoryView.as_view(), name='SiteHistory',
-       kwargs={'model': Site}),
+        kwargs={'model': Site}),
 
-    url(r'^json/(?P<pk>\d+)/$', 'xsd_sites.views.sitedetail_json', name='sitedetail_json'),
-)
+    url(r'^json/(?P<pk>\d+)/$', sitedetail_json, name='sitedetail_json'),
+]
