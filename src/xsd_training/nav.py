8851c57ab37cd81a4c00ed7046d0b3e33bda7489
Will Pimblett <will@wjdp.uk>
2017-06-18 16:54:34 +0100

    Add Trainee Note pages to nav extra pages under 'Trainee Profiles'

--- src/xsd_training/nav.py
+++ src/xsd_training/nav.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from xSACdb.roles.functions import *
 
 NAV = (
@@ -15,7 +17,8 @@ NAV = (
         'access': is_verified,
         'items': [
             ('All SDCs', None, 'fa fa-list', 'xsd_training:SDCList', []),
-            ('Upcoming Courses', None, 'fa fa-calendar', 'xsd_training:PerformedSDCList', ['xsd_training:PerformedSDCDetail']),
+            ('Upcoming Courses', None, 'fa fa-calendar', 'xsd_training:PerformedSDCList',
+             ['xsd_training:PerformedSDCDetail']),
         ]
     },
     {
@@ -23,7 +26,15 @@ NAV = (
         'access': is_instructor,
         'items': [
             ('Lessons', None, 'fa fa-clock-o', 'xsd_training:InstructorUpcoming', []),
-            ('Trainee Profiles', None, 'fa fa-pencil-square-o', 'xsd_training:TraineeNotesSearch', []),
+            ('Trainee Profiles', None, 'fa fa-pencil-square-o', 'xsd_training:TraineeNotesSearch',
+             ['xsd_training:TraineeNotes'
+              'xsd_training:TraineeLessonDetail'
+              'xsd_training:TraineePerformedLessonCreate'
+              'xsd_training:TraineePerformedLessonUpdate'
+              'xsd_training:TraineePerformedLessonDelete'
+              'xsd_training:TraineeQualificationCreate'
+              'xsd_training:TraineeQualificationUpdate'
+              'xsd_training:TraineeQualificationDelete']),
         ]
     },
     {
@@ -31,8 +42,8 @@ NAV = (
         'access': is_training,
         'items': [
             ('Groups', None, 'fa fa-users', 'xsd_training:TraineeGroupList', ['xsd_training:TraineeGroupCreate',
-                                                                        'xsd_training:TraineeGroupUpdate',
-                                                                        'xsd_training:TraineeGroupDelete',]),
+                                                                              'xsd_training:TraineeGroupUpdate',
+                                                                              'xsd_training:TraineeGroupDelete', ]),
             ('Progress Report', None, 'fa fa-flag-checkered', 'xsd_training:TraineeGroupProgress', []),
             ('Update Requests', None, 'fa fa-envelope-o', 'xsd_training:TrainingUpdateRequestList', []),
         ]
@@ -42,8 +53,8 @@ NAV = (
         'access': is_training,
         'items': [
             ('Session Planning', None, 'fa fa-calendar', 'xsd_training:SessionList', ['xsd_training:SessionCreate',
-                                                                                'xsd_training:SessionPlanner',
-                                                                                'xsd_training:SessionDelete',]),
+                                                                                      'xsd_training:SessionPlanner',
+                                                                                      'xsd_training:SessionDelete', ]),
             ('Create Pool Sheet', None, 'fa fa-file-o', 'xsd_training:PoolSheet', []),
             ('Retrospectively Add Lessons', None, 'fa fa-table', 'xsd_training:RetroAddLessons', []),
         ]
