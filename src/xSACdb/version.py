e584ee63194b1b5e76f5780f04d00feeaeda00db
Will Pimblett <will@wjdp.uk>
2019-01-12 23:05:33 +0000

    Bump version to v0.6.0, should really be automatic

--- src/xSACdb/version.py
+++ src/xSACdb/version.py
@@ -1,6 +1,7 @@
 # Version file, currently manually updated
+# TODO: This really should be automatic
 
 VERSION = {
-    'tag': 'v0.5.3',
-    'released': '2017-06-18',
+    'tag': 'v0.6.0',
+    'released': '2019-01-12',
 }
