7c72fde4224fe3679148e18d8585208c9705acff
Will Pimblett <will@wjdp.uk>
2019-11-03 16:14:54 +0000

    Set up sentry-sdk

--- src/xSACdb/settings/production.py
+++ src/xSACdb/settings/production.py
@@ -2,7 +2,6 @@
 
 import socket
 import dj_database_url
-from raven.transport import RequestsHTTPTransport
 
 from .common import *
 
@@ -35,10 +34,17 @@ from xSACdb.version import VERSION
 DEFAULT_FROM_EMAIL = EMAIL_FROM
 
 if 'RAVEN_CONFIG' in locals():
-    RAVEN_CONFIG['release'] = VERSION['tag']
-    RAVEN_CONFIG['site'] = CLUB['name']
-    # Async transport has issues with RQ http://python-rq.org/patterns/sentry/
-    RAVEN_CONFIG['transport'] = RequestsHTTPTransport
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=RAVEN_CONFIG['dsn'],
+        integrations=[DjangoIntegration()],
+        release= VERSION['tag'],
+        environment='staging' if STAGING else 'production',
+    )
+    with sentry_sdk.configure_scope() as scope:
+        scope.set_tag("site", CLUB['name'])
 else:
     RAVEN_CONFIG = {}
 
@@ -86,11 +92,6 @@ TEMPLATES[0]['OPTIONS']['loaders'] = (
 # Cache sessons (reads only, writes still go to database)
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
-# Catch 404s
-MIDDLEWARE_CLASSES = (
-    'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
-) + MIDDLEWARE_CLASSES
-
 # Webpack
 WEBPACK_LOADER['DEFAULT']['CACHE'] = True
 
@@ -99,7 +100,6 @@ LOGGING = {
     'disable_existing_loggers': True,
     'root': {
         'level': 'WARNING',
-        'handlers': ['sentry'],
     },
     'formatters': {
         'rq_console': {
@@ -108,10 +108,6 @@ LOGGING = {
         },
     },
     'handlers': {
-        'sentry': {
-            'level': 'WARNING', # To capture more than ERROR, change to WARNING, INFO, etc.
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-        },
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
@@ -133,18 +129,8 @@ LOGGING = {
             'handlers': ['console'],
             'propagate': False,
         },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
         'rq.worker': {
-            'handlers': ['rq_console', 'sentry'],
+            'handlers': ['rq_console'],
             'level': 'DEBUG'
         },
     }
