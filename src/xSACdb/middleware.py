c1a739ea91b1fc8a048ef35dc366f23a2db76193
Tom Price <tom@codedinternet.com>
2019-11-06 17:34:44 +0000

    Update our custom middleware to the new Django 1.10+ structure.
    Convert is_authenticated from a method call to a parameter.

--- src/xSACdb/middleware.py
+++ src/xSACdb/middleware.py
@@ -12,7 +12,7 @@ if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
     EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
 
 
-class LoginRequiredMiddleware:
+class LoginRequiredMiddleware(object):
     """
     Middleware that requires a user to be authenticated to view any page other
     than LOGIN_URL. Exemptions to this requirement can optionally be specified
@@ -23,19 +23,62 @@ class LoginRequiredMiddleware:
     loaded. You'll get an error if they aren't.
     """
 
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
         assert hasattr(request, 'user')
-        if not request.user.is_authenticated():
+        if not request.user.is_authenticated:
             path = request.path_info.lstrip('/')
             if not any(m.match(path) for m in EXEMPT_URLS):
                 return HttpResponseRedirect(settings.LOGIN_URL + '?next=/{}'.format(path))
 
+        response = self.get_response(request)
 
-class NewbieProfileFormRedirectMiddleware:
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+
+class NewbieProfileFormRedirectMiddleware(object):
     """
     Redirect new users to the newbie form if they're missing details from
     their profile.
     """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        assert hasattr(request, 'user')
+
+        CACHE_KEY = self.get_cache_key(request.user)
+        path = request.path_info.lstrip('/')
+
+        if not (cache.get(CACHE_KEY) is True or (m.match(path) for m in EXEMPT_URLS)):
+
+            if request.user.is_authenticated and request.user.memberprofile.get_missing_field_list() != []:
+                newbie_form_url = reverse('xsd_members:MemberProfileUpdate')
+                if request.path_info != newbie_form_url:
+                    return HttpResponseRedirect(newbie_form_url)
+            else:
+                cache.set(CACHE_KEY, True, 3600)
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
 
     @classmethod
     def get_cache_key(cls, user):
@@ -44,22 +87,6 @@ class NewbieProfileFormRedirectMiddleware:
     @classmethod
     def invalidate_cache(cls, user):
         cache.delete(cls.get_cache_key(user))
-
-    def process_request(self, request):
-        assert hasattr(request, 'user')
-
-        CACHE_KEY = self.get_cache_key(request.user)
-        path = request.path_info.lstrip('/')
-
-        if cache.get(CACHE_KEY) == True or any(m.match(path) for m in EXEMPT_URLS):
-            return
-
-        if (request.user.is_authenticated() and request.user.memberprofile.get_missing_field_list() != []):
-            newbie_form_url = reverse('xsd_members:MemberProfileUpdate')
-            if request.path_info != newbie_form_url:
-                return HttpResponseRedirect(newbie_form_url)
-        else:
-            cache.set(CACHE_KEY, True, 3600)
 
 
 def dev_show_toolbar(request):
