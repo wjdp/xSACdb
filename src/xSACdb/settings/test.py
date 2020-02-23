3a8b2da1c0f30475b71da914c52fb79b5a53d54a
Tom Price <tom@codedinternet.com>
2020-02-23 03:10:49 +0000

    Fix missing postgres config env vars.
    These were failing in !80, appears to be the cause according to the logs.
    
    See also: https://docs.gitlab.com/ee/ci/services/postgres.html

--- src/xSACdb/settings/test.py
+++ src/xSACdb/settings/test.py
@@ -1,9 +1,8 @@
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+# noinspection PyUnresolvedReferences
 from .common import *
 
 ALLOWED_HOSTS = ['*']
@@ -31,10 +30,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'xsacdb',  # Or path to database file if using sqlite3.
         'USER': 'runner',  # Not used with sqlite3.
-        'PASSWORD': '',  # Not used with sqlite3.
+        'PASSWORD': 'runner-password',  # Not used with sqlite3.
         'HOST': 'postgres',  # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',  # Set to empty string for default. Not used with sqlite3.
     }
